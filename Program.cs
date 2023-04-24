828ee238fa576acc648fb655f8c54c4350628d28
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2023-04-24 15:36:35 -0300

    Inclusão do Swagger/OpenAPI no projeto

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Inject Swagger Services 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => { });
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Use Swagger in application. 
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+// Sample Endpoint
+app.MapGet("/", () => "Hello! This is .NET 6 Minimal API.   /swagger to get Endpoints doc.");
 
 app.Run();
